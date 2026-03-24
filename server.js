0938774d4c5dd49d2bd0d4d4edcb3e44ede0ee6b
fatimangoungoure20-coder <fatimangoungoure20@gmail.com>
2026-03-24 17:38:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,1 @@
-const  http = require("http");
-const server = http.createserver((req,res) =>{
-res.end("mon API fonctionne !");
-});
-seveur.listen(3000, ()=> {
-console.log("serveur lance sur le port 3000");
-});
+
